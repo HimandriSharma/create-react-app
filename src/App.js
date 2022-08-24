34c7b959b806bde2560f7436e69097ee868c148d
Himandri Sharma <himandrisharma@Himandris-MacBook-Pro.local>
2022-08-24 21:27:13 +0530

    sourcemap stated false

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import React,{useEffect} from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+  const onKeyDown = (e) => {
+    if(e.metaKey && e.which === 13) {
+      console.log("command + enter clicked");
+    }
+  }
+  
+  useEffect(() => {
+    document.getElementById("test").addEventListener('keydown', onKeyDown);
+    
+    return () => {
+        document.getElementById("test").removeEventListener('keydown', onKeyDown);
+    }
+  });
+  
+  return <textarea id="test"/>
 }
 
 export default App;
+// import logo from './logo.svg';
+// import './App.css';
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
